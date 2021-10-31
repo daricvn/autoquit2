26d3a4972a3b9a9663ef96bcd2604381e3bb12b9
Darick Nguyen <daricvn@gmail.com>
2021-10-31 20:27:02 +0700

    Update compiler

--- src/Autoquit.DynamicModule/ExpressionCompiler.cs
+++ src/Autoquit.DynamicModule/ExpressionCompiler.cs
@@ -20,18 +20,16 @@ namespace Autoquit.DynamicModules
 
         private ObjectActivator<T> GetActivator<T>(ConstructorInfo ctor)
         {
-            Type type = ctor.DeclaringType;
             ParameterInfo[] paramsInfo = ctor.GetParameters();
 
-            //create a single param of type object[]
+            // Create a single param of type object[]
             ParameterExpression param =
                 Expression.Parameter(typeof(object[]), "args");
 
             Expression[] argsExp =
                 new Expression[paramsInfo.Length];
 
-            //pick each arg from the params array 
-            //and create a typed expression of them
+            // Look up parameters
             for (int i = 0; i < paramsInfo.Length; i++)
             {
                 Expression index = Expression.Constant(i);
@@ -46,16 +44,12 @@ namespace Autoquit.DynamicModules
                 argsExp[i] = paramCastExp;
             }
 
-            //make a NewExpression that calls the
-            //ctor with the args we just created
             NewExpression newExp = Expression.New(ctor, argsExp);
 
-            //create a lambda with the New
-            //Expression as body and our param object[] as arg
             LambdaExpression lambda =
                 Expression.Lambda(typeof(ObjectActivator<T>), newExp, param);
 
-            //compile it
+            // Compile it
             ObjectActivator<T> compiled = (ObjectActivator<T>)lambda.Compile();
             return compiled;
         }
