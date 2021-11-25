66a83e6cd8a90fac4fc15293a90f8e4da63083f7
gtdcomputer <86706306+gtdcomputer@users.noreply.github.com>
2021-11-25 19:42:25 +0700

    create chromely view
    -Add EmblemdedResources bind to webview folder
    - Create chromely forms

--- src/Autoquit2.Core/Program.cs
+++ src/Autoquit2.Core/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Autoquit2.Core.Modules;
 using Chromely.Core;
+using Chromely.Core.Configuration;
+using Chromely.Core.Network;
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Autoquit2.Core
 {
@@ -8,8 +12,35 @@ namespace Autoquit2.Core
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Press any key to continue...");
-            Console.Read();
+            //Console.WriteLine("Press any key to continue...");
+            //Console.Read();
+            System.Threading.Thread thread = new System.Threading.Thread(() =>
+            {
+                AppBuilder.Create()
+                    .UseApp<myChromelyApp>()
+                    .UseConfig<DefaultConfiguration>(new DefaultConfiguration()
+                    {
+                        CefDownloadOptions = new CefDownloadOptions(true, true),
+                        StartUrl = "app://autoquit2/index.html",
+                        DebuggingMode = true,
+                        WindowOptions = new WindowOptions()
+                        {
+                            Title = "Autoquit2",
+                            DisableResizing = true,
+                            RelativePathToIconFile = "fav.ico",
+                            StartCentered = true,
+                            DisableMinMaximizeControls = true,
+                            Size = new WindowSize(1080,600)
+                        },
+                        UrlSchemes = new List<UrlScheme>()
+                        {
+                            new UrlScheme("app", "app", "autoquit2", string.Empty, UrlSchemeType.AssemblyResource, assemblyOptions: new AssemblyOptions(Assembly.GetExecutingAssembly(),"Autoquit2.Core", ""))
+                        }
+                    })
+                    .Build()
+                    .Run(args);
+            });
+            thread.Start();
         }
 
         private static void RunApp(IModuleManager moduleManager)
